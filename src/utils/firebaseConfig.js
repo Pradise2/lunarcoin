42095fbf9d4a9b40a58495c2c12c7802247acd53
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-08-01 00:41:55 +0100

    Initial commit

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -3,17 +3,15 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore'; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAMXy8cfauXHzfhHIp3bDAOP-nFLM6xrHs",
-    authDomain: "thel-ed136.firebaseapp.com",
-    projectId: "thel-ed136",
-    storageBucket: "thel-ed136.appspot.com",
-    databaseURL: "https://thel-ed136-default-rtdb.firebaseio.com",
-    messagingSenderId: "18007041500",
-    appId: "1:18007041500:web:182cb1eb7e039968165b19",
-    measurementId: "G-BYHJQDVC2Y"
-  
-};
-
+    apiKey: "AIzaSyBoapqfEEJwsZiStOWhZeTZlpFStKFCY80",
+    authDomain: "lunar-2ac46.firebaseapp.com",
+    databaseURL: "https://lunar-2ac46-default-rtdb.firebaseio.com",
+    projectId: "lunar-2ac46",
+    storageBucket: "lunar-2ac46.appspot.com",
+    messagingSenderId: "954289049346",
+    appId: "1:954289049346:web:1a08d54b3ae4122c82fc1b",
+    measurementId: "G-57Q2844SHQ"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
