b468182cbb87d1ba8e3c25dce49b99b1cb7f7fd2
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-09-02 09:23:31 +0100

    Initial commit

--- src/Pages/Squad.js
+++ src/Pages/Squad.js
@@ -2,11 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Footer from '../Component/Footer';
 import { ClipLoader } from 'react-spinners';
 import './bg.css';
-import { motion, AnimatePresence } from 'framer-motion';
 import axios from 'axios';
-import user from './user.png';
-import './bgvh.css';
-
 
 const Squad = () => {
   const [copied, setCopied] = useState(false);
@@ -14,19 +10,14 @@ const Squad = () => {
   const [username, setUserName] = useState(null);
   const [userSquad, setUserSquad] = useState(null);
   const [squads, setSquads] = useState([]);
-  const [all, setAll] = useState([]);
   const [loading, setLoading] = useState(true);
   const [showRCSquad, setShowRCSquad] = useState(false);
-  const [error, setError] = useState(null); // Define error state
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Check if Telegram WebApp is available
     if (window.Telegram && window.Telegram.WebApp) {
       const { WebApp } = window.Telegram;
-
-      // Expand the WebApp
       WebApp.expand();
-
       const user = WebApp.initDataUnsafe?.user;
       if (user) {
         setUserId(user.id);
@@ -39,58 +30,25 @@ const Squad = () => {
     }
   }, []);
 
-  useEffect(() => { 
-    const fetchSquadData = async () => {
+  useEffect(() => {
+    const fetchData = async () => {
       try {
-        const response = await axios.get(`https://lunarapp.thelunarcoin.com/backend/api/squad/${userId}`);
-        const { userSquad, squads } = response.data;
+        const squadResponse = await axios.get(`https://lunarapp.thelunarcoin.com/backend/api/squad/${userId}`);
+        const { userSquad, squads } = squadResponse.data;
         setUserSquad(userSquad);
-        setSquads(squads || []); 
-        console.log('Fetched squad data:', { userSquad, squads }); 
+        setSquads(squads || []);
       } catch (error) {
-        console.error('Error fetching squad data:', error);
-        setError('Error fetching squad data');
+        console.error('Error fetching data:', error);
+        setError('Error fetching data');
       } finally {
         setLoading(false);
       }
     };
 
     if (userId) {
-      fetchSquadData();
+      fetchData();
     }
   }, [userId]);
-
- useEffect(() => { 
-    const fetchAllData = async () => {
-      try {
-        const response = await axios.get(`https://lunarapp.thelunarcoin.com/backend/api/getuserbackup/${userId}`);
-        const all = response.data;
-        
-        // Calculate Balance
-        const dailyBalance = parseFloat(all.dailyBalance);
-        const specialBalance = parseFloat(all.specialBalance);
-        const initialFarmBalance = parseFloat(all.initialFarmBalance);
-        const farmClaimCount = parseInt(all.farmClaimCount);
-  
-        const balance = dailyBalance + specialBalance + initialFarmBalance + (farmClaimCount * 14400);
-        setAll({ ...all, balance });
-  
-        console.log('Fetched all data:', { ...all, balance });
-      } catch (error) {
-        console.error('Error fetching all data:', error.message);
-        setError(`Failed to fetch data: ${error.message}`);
-      } finally {
-        setLoading(false);
-      }
-    };
-  
-    if (userId) {
-      setLoading(true);
-      fetchAllData();
-    }
-  }, [userId]);
-  
-
 
   const copyToClipboard = () => {
     const reflink = `https://t.me/ThelunarCoin_bot?start=ref_${userId}`;
@@ -118,25 +76,17 @@ const Squad = () => {
     }
   };
 
- 
-
   const handleClaim = async (userId) => {
     if (navigator.vibrate) {
       navigator.vibrate(500); // Vibrate for 500ms
     }
-  
+
     const earning = Number(userSquad?.referralCount || 0) * 5000;
     const difference = Number(earning) - Number(userSquad?.claimedReferral || 0);
     const newClaimedReferral = Number(userSquad?.claimedReferral || 0) + Number(difference);
     const newTotalSquad = Number(userSquad?.totalBalance || 0) + Number(difference);
-    const totalBalance = Number(all?.balance || 0) + Number(newTotalSquad);
-  
-    console.log('Earning:', earning);
-    console.log('Difference:', difference);
-    console.log('New Claimed Referral:', newClaimedReferral);
-    console.log('New Total Squad:', newTotalSquad);
-    console.log('Total Balance:', totalBalance);
-  
+    const totalBalance = Number(newTotalSquad);
+
     try {
       const response = await axios.put(`https://lunarapp.thelunarcoin.com/backend/api/squad/update`, {
         userId: userId,
@@ -144,60 +94,28 @@ const Squad = () => {
         totalbalance: totalBalance.toFixed(2),
         totalsquad: newTotalSquad,
       });
-  
-      console.log('Claim response:', response.data);
-  
-      // Re-fetch updated squad and farm data
+
       const updatedSquadResponse = await axios.get(`https://lunarapp.thelunarcoin.com/backend/api/squad/${userId}`);
       const updatedSquad = updatedSquadResponse.data.userSquad;
-     console.log('Claim update response:', updatedSquadResponse.data);
-  
-   
 
       setUserSquad(updatedSquad);
-   } catch (error) {
+    } catch (error) {
       console.error('Error during claim:', error);
       setError('Error during claim');
     }
-  
+
     setShowRCSquad(true);
     setTimeout(() => setShowRCSquad(false), 2000);
   };
-  
 
-  if (loading) {
-    return (
-      <div className="min-h-screen flex justify-center items-center bg-cover text-white p-4">
-        <div className="flex flex-col items-center space-y-4">
-          <h1 className="text-white text-4xl font-normal">
-            <ClipLoader
-              color="#FFD700" // Golden color
-              size={60}
-              speedMultiplier={1}
-            />
-          </h1>
-        </div>
-      </div>
-    );
-  }
-
-  const totalBalance = Number(all?.balance || 0) + Number(userSquad?.totalSquad || 0);
-  const displayTotalBalance = totalBalance.toLocaleString('en-US', {
+  const displayTotalBalance = Number(userSquad?.claimedReferral || 0).toLocaleString('en-US', {
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   });
 
-
-  console.log('TotalBal', totalBalance);
-  console.log('Total', all?.balance);
-  
-  // Format the difference with commas and two decimal places
- 
-
   const earning = Number(userSquad?.referralCount || 0) * 5000;
   const difference = Number(earning) - Number(userSquad?.claimedReferral || 0);
-  const newTotalSquad = Number(userSquad?.totalBalance || 0) + Number(difference);
-    
+
   const formattedDifference = difference.toLocaleString('en-US', {
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
@@ -205,21 +123,36 @@ const Squad = () => {
 
   const isClaimable = difference > 0;
 
-  console.log('Earning:', earning);
-    console.log('Difference:', difference);
-    console.log('Total Balance:', totalBalance);
-    console.log('new Balance:', newTotalSquad);
-  
+  if (loading) {
+    return (
+      <div
+        className="relative min-h-screen bg-black bg-blur-sm bg-don bg-center bg-no-repeat text-white flex items-center justify-center p-4 space-y-4"
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-60"></div>
+        <div
+          className="absolute transform -translate-y-1/2 top-1/2 flex justify-center items-center"
+          style={{ top: '50%' }}
+        >
+          <ClipLoader
+            color="#FFD700"
+            size={100}
+            speedMultiplier={1}
+          />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div
-    className="relative min-h-screen bg-black bg-blur-sm bg-don bg-[center_top_5rem] bg-no-repeat text-white flex flex-col items-center p-4 space-y-4 ">
+      className="relative min-h-screen bg-black bg-blur-sm bg-don bg-[center_top_5rem] bg-no-repeat text-white flex flex-col items-center p-4 space-y-4 ">
       <div className="absolute inset-0 bg-black bg-opacity-60"></div>
-    <h1 className="text-center text-4xl font-normal relative z-10">
-     The bigger the tribe, <br /> the better the vibe!
+      <h1 className="text-center text-4xl font-normal relative z-10">
+        The bigger the tribe, <br /> the better the vibe!
       </h1>
       <p className='relative text-fin font-normal text-center items-center'>
-      But hey, only qualification actions unlock <br/>
-      <span className='text-fin font-bold'>The LAR galaxy!</span>
+        But hey, only qualification actions unlock <br />
+        <span className='text-fin font-bold'>The LAR galaxy!</span>
       </p>
       <div className="relative bg-sinc bg-opacity-10 p-4  rounded-xl w-full max-w-md space-y-2">
         <p className="text-zinc-400 text-center">Total squad balance</p>
@@ -230,23 +163,23 @@ const Squad = () => {
       <div className="relative bg-sinc bg-opacity-10 p-4 rounded-xl w-full max-w-md space-y-2">
         <p className="text-zinc-400 text-center">Your rewards</p>
         <p className="text-center text-3xl font-bold">
-         {formattedDifference} <span className="text-golden-moon font-bold">LAR</span>
+          {formattedDifference} <span className="text-golden-moon font-bold">LAR</span>
         </p>
         <p className="text-sm mb-4 text-center">Earn 5,000 LAR per Referral</p>
         <div className="flex p-1 justify-center">
           <button
             className={`px-4 py-2 rounded-xl ${isClaimable ? 'bg-golden-moon text-white ' : 'bg-gray-500 text-white'}`}
-            onClick={isClaimable ? handleClaim : null}
+            onClick={() => isClaimable ? handleClaim(userSquad?.userId) : null}
             disabled={!isClaimable}
           >
             {isClaimable ? 'Claim' : 'Claimed'}
           </button>
         </div>
       </div>
-      <div className= "relative bg-sinc bg-opacity-10 p-4 rounded-xl w-full max-w-md space-y-2">
+      <div className="relative bg-sinc bg-opacity-10 p-4 rounded-xl w-full max-w-md space-y-2">
         <div className="flex justify-between bg-CharcoalGray bg-opacity-70 text-sm items-center bg-zinc-700 rounded-lg py-2 px-3">
           <p className="flex items-center">
-            <img aria-hidden="true" alt="team-icon" src={user} className="mr-2" />
+            <img aria-hidden="true" alt="team-icon" src="https://openui.fly.dev/openui/24x24.svg?text=👥" className="mr-2" />
             Your team
           </p>
           <p>{userSquad?.referralCount} users</p>
@@ -256,28 +189,26 @@ const Squad = () => {
             squads.map((referral, index) => (
               <div key={index} className="flex font-normal text-sm justify-between items-center px-3">
                 <p className="flex items-center">
-                  <img aria-hidden="true" alt="user-icon"  src={user} className="mr-2" />
+                  <img aria-hidden="true" alt="user-icon" src="https://openui.fly.dev/openui/24x24.svg?text=👤" className="mr-2" />
                   {referral.newUserName}
                 </p>
-                <p className="text-primary">{referral.referralEarning} <span className="text-golden-moon">LAR</span></p>
+                <p className="text-primary">{referral.referralEarning} <span className="text-fin">LAR</span></p>
               </div>
             ))
-          ) : (  
-            <p className="text-center text-sm text-zinc-400">No referrals yet.</p>
+          ) : (
+            <p className="text-center text-zinc-400 text-sm">No referrals yet. Share your link and start earning!</p>
           )}
-        </div> 
+        </div>
       </div>
-      <div className="relative w-full max-w-md flex space-x-2 mt-5">
-        <button className="flex-1 bg-custom py-2 rounded-lg" onClick={copyToClipboard}>
-          Invite friends
+      <div className="relative bg-sinc bg-opacity-10 p-4 rounded-xl w-full max-w-md space-y-2">
+        <button
+          className={`px-4 py-2 rounded-xl ${copied ? 'bg-golden-moon text-white ' : 'bg-zinc-500 text-white'}`}
+          onClick={copyToClipboard}
+        >
+          {copied ? 'Link Copied!' : 'Copy Referral Link'}
         </button>
-        <button className="bg-sinc bg-opacity-10 p-2 rounded-lg" onClick={copyToClipboard}>
-          {copied ? <span>Copied!</span> : <span>Copy</span>}
-        </button>
       </div>
-      <div className="w-full max-w-md sticky bottom-0 left-0 flex text-white bg-zinc-900 justify-around py-1">
-        <Footer />
-      </div>
+      <Footer />
     </div>
   );
 };
